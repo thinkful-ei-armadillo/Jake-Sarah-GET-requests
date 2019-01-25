987cbf3b457bedf3ab8648ccad1e7c78515e9acc
Sarah Drew <sarahdrew93@gmail.com>
2019-01-25 14:04:05 -0600

    completed dog breed search

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function getDogImage(picAmount) {
     .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
+      
     .catch(error => alert('Something went wrong in else. Try again later.'));
 }
 
@@ -20,19 +21,51 @@ function displayResults(responseJson) {
 
   $('.display-all-dogs').html(displayDogs);
   //display the results section
+console.log(responseJson.message)
+
   $('.results').removeClass('hidden');
 }
 
 
 function watchForm() {
-  $('form').submit(event => {
+  $('.number-form').submit(event => {
     event.preventDefault();
     const picAmount = $('.number').val();
     getDogImage(picAmount);
   });
 }
 
+function getDogBreed(dogBreed){
+  fetch(`https://dog.ceo/api/breed/hound-${dogBreed}/images/random`)
+  //console.log(`https://dog.ceo/api/breeds/hound/images/random`)
+  .then(response => response.json())
+  .then(responseJson =>
+    displayDogBreed(responseJson))
+   
+  .catch(error => alert('Breed not found.'))
+
+}
+
+function breedForm(){
+  $('.breed-form').submit(event => {
+    event.preventDefault();
+    const dogBreed= $('.dog-breed').val();
+    console.log(dogBreed);
+    getDogBreed(dogBreed);
+  });
+}
+
+function displayDogBreed(responseJson){
+  const displayBreed =[];
+  displayBreed.push(`<img src="${responseJson.message}">`)
+  $('.display-all-dogs').html(displayBreed);
+  $('.results').removeClass('hidden');
+console.log(responseJson.message)
+}
+
 $(function() {
   console.log('App loaded! Waiting for submit!');
   watchForm();
+  breedForm();
 });
+
